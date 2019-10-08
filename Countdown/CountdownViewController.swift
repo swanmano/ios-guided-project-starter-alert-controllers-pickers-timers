6a9e656716637fbc410f76f8c9464c77c15817fa
Craig Swanson <cswanson02@me.com>
2019-10-07 21:09:57 -0500

    Finished setting up the user interface

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -38,7 +38,7 @@ class CountdownViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
-        
+        showAlert()
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
@@ -48,7 +48,10 @@ class CountdownViewController: UIViewController {
     // MARK: - Private
     
     private func showAlert() {
+        let alert = UIAlertController(title: "Timer Finished!", message: "Your countdown is over.", preferredStyle: .alert)
         
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     private func updateViews() {
